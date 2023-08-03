69b7dd710c18151734aa9fae9fc29db6900b034d
Divany <harryndira@gmail.com>
2023-08-03 12:28:54 +0700

    Initialize app

---
+++ src/app/api/articles/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const res = await fetch(process.env.BASE_URL + "/articles", {
+        headers: {
+            Authorization : process.env.TOKEN,
+        }
+    },)
+
+    const data = await res.json();
+    return NextResponse.json({data});
+}
